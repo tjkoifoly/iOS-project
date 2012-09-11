1076f118b2d5f8a04e0c4adb5cbdd6cb02da10c8
Nguyen Chi Cong <foly.v1.8.01@gmail.com>
2012-09-11 20:35:56 +0700

    init empty project

---
+++ Sources code/Wallets Manager/Wallets Manager/folyAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  folyAppDelegate.h
+//  Wallets Manager
+//
+//  Created by Nguyen Chi Cong on 9/11/12.
+//  Copyright (c) 2012 BKHN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface folyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Sources code/Wallets Manager/Wallets Manager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wallets Manager
+//
+//  Created by Nguyen Chi Cong on 9/11/12.
+//  Copyright (c) 2012 BKHN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "folyAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([folyAppDelegate class]));
+    }
+}
